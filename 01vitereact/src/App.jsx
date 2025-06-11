bd6f97a952c29b416bad17ce3813848ab5c359f7
neeraj07-web <neerajiiit07@gmail.com>
2025-06-11 15:54:23 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>hello chai and react with vite | Neeraj Kumar</h1>
+  )
+}
+
+export default App
--- REACT AND CODE/readme.md
+++
@@ -1,1 +0,0 @@
-// hello in this readme file we are going to write about the documentation of the react
